79ffb7c962118014d3e9f51f192d969ed6547508
Chris Smith <256821015+mo-cjsmith@users.noreply.github.com>
2026-08-25 11:05:36 +0100

    Update upgrade macro

--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -18,11 +18,11 @@ class UpgradeError(Exception):
     __str__ = __repr__
 
 
-class vn31_t226(MacroUpgrade):
+class vn32_t226(MacroUpgrade):
     """Upgrade macro for ticket #226 by Chris Smith."""
 
-    BEFORE_TAG = "vn3.1"
-    AFTER_TAG = "vn3.1_t226"
+    BEFORE_TAG = "vn3.2"
+    AFTER_TAG = "vn3.2_t226"
 
     def upgrade(self, config, meta_config=None):
         self.add_setting(config, ["namelist:initial_temperature", "profile_variable"], "'potential'")
