30299b261cfd1541ccb3fd8fdd375971eab92524
Chris Smith <256821015+mo-cjsmith@users.noreply.github.com>
2026-09-02 12:49:57 +0100

    Label upgrade macro with PR number

--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -18,11 +18,11 @@ class UpgradeError(Exception):
     __str__ = __repr__
 
 
-class vn32_t226(MacroUpgrade):
-    """Upgrade macro for ticket #226 by Chris Smith."""
+class vn32_t618(MacroUpgrade):
+    """Upgrade macro for PR #618 by Chris Smith."""
 
     BEFORE_TAG = "vn3.2"
-    AFTER_TAG = "vn3.2_t226"
+    AFTER_TAG = "vn3.2_t618"
 
     def upgrade(self, config, meta_config=None):
         self.add_setting(config, ["namelist:initial_temperature", "profile_variable"], "'potential'")
